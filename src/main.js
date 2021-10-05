61e3c014feca716c193f9806cefc56b7fbbf91d8
cxphoenix <jate9229@gmail.com>
2021-10-05 11:49:13 +0800

    update: content and modal template and function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
 import './index.css'
+
+function windowModal(status=true, {title, body, checkBtnContent, cancelBtnContent, haveCancel, checkBtnAction}) {
+    const modal = document.querySelector('#modal');
+    if (!status) {
+        modal.classList.add('hidden');
+        return;
+    }
+    modal.classList.remove('hidden');
+    const modalTitle = document.querySelector('#modalTitle');
+    const modalBody = document.querySelector('#modalBody');
+    const modalCheckBtn = document.querySelector('#modalCheckBtn');
+    const modalCancelBtn = document.querySelector('#modalCancelBtn');
+    if (!haveCancel) {
+        modalCancelBtn.classList.add('hidden');
+    }
+    modalTitle.innerText = title;
+    modalBody.innerText = body;
+    modalCheckBtn.innerText = checkBtnContent;
+    modalCancelBtn.innerText = cancelBtnContent;
+
+    modalCheckBtn.addEventListener('click', checkBtnAction);
+}
+
+const passInput = document.querySelector('#pass');
+
+const submitBtn = document.querySelector('#pwdSubmit');
+submitBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    const warningImg = document.querySelector('#warning-img');
+    if (passInput.value === "") {
+        warningImg.classList.remove('hidden');
+        windowModal(true, {title: 'testing', body: 'test', checkBtnContent: '確認', cancelBtnContent: '取消', haveCancel: true})
+        return;
+    }
+})
